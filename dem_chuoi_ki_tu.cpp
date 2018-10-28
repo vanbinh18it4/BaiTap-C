4828286719704d6d8796539d60e479def7783cae
= <=>
2018-10-28 18:01:14 +0700

    commit to repository

---
+++ dem_chuoi_ki_tu.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int
+ main(){
+	char chuoi[100];
+	int dem;
+	printf("Moi ban nhap 1 chuoi: ");
+	scanf("%s",&chuoi);
+	dem= strlen(chuoi);
+	printf("co %d ki tu.",dem);
+}
